430e9441cf608144d1eeaa08fd07d40911effe3c
Shruti Sharma <shruti12101997@gmail.com>
2022-12-17 15:29:01 -0500

    Image Generator.py changed

--- modules/ImageGenerator.py
+++ modules/ImageGenerator.py
@@ -4,83 +4,89 @@ from matplotlib import pyplot as plt
 class ImageGenerator(object):
 
     def __init__(self, x, y):
-        """
-        Initialize an ImageGenerator instance.
-        :param x: A Numpy array of input data. It has shape (num_of_samples, height, width, channels).
-        :param y: A Numpy vector of labels. It has shape (num_of_samples, ).
-        """
+        #instance for imageGenerator
+        # param x ---------- a numpy array for inputs, has a shape - number of sample, height, width and channels
+        # param y ---------- a numpy vector for labels, shape ----- number of samples
         self.samples_count
         self.x = x
         self.y = y
         self.sample_count, self.height, self.width, self.channel_count = self.x.shape  # N,H,W,C
-        self.height_shift = 0  # the amount shift in height
-        self.width_shift = 0  # the amount shift in width
-        self.angle = 0  # default no angle
-        self.flip_horizontal = False   # whether a horizontal flip is needed
-        self.flip_vertical = False  # whether a vertical flip is needed
-        self.add_noise = False  # whether to add noise
+        # height shift
+        self.height_shift = 0  
+        # width shift
+        self.width_shift = 0 
+        # default  ----- no angle 
+        self.angle = 0  
+        # need a horizontal flip
+        self.flip_horizontal = False  
+        # need a vertical flip
+        self.flip_vertical = False  
+        #add noise or not
+        self.add_noise = False
 
-    def next_batch_gen(self, batch_size, shuffle=True):
+    def next_batch_gen(self, batchSize, shuffle=True):
         """
-        A python generator function that yields a batch of data indefinitely.
-        :param batch_size: The number of samples to return for each batch.
-        :param shuffle: If True, shuffle the entire dataset after every sample has been returned once.
-                        If False, the order or data samples stays the same.
-        :return: A batch of data with size (batch_size, width, height, channels).
+        a generator function to yield a data batch indefinitely 
+        param 
+            batch_size: number of samples to be returned for each batch
+            shuffle: If True ----- shuffle entire dataset after every sample has been returned once
+                        If False ----- order/data sample stays same
+        :return: A batch of data with size ---- batch, size, width, height, channels
         """
-        total_batch_num = self.sample_count // batch_size  # number of batches in total
-        batch_num = 0  # type = int
+        #total number of batches
+        totalBatchNum = self.sampleCount
+        batchNum = 0
         while True:
-            if batch_num < total_batch_num:
-                batch_num += 1
-                yield (self.x[(batch_num - 1) * batch_size : batch_num * batch_size],
-                       self.y[(batch_num - 1) * batch_size : batch_num * batch_size])
+            if batchNum < totalBatchNum:
+                batchNum += 1
+                yield (self.x[(batchNum - 1) * batchSize : batchNum * batchSize],
+                       self.y[(batchNum - 1) * batchSize : batchNum * batchSize])
             else:
                 if shuffle:
-                    perm = np.random.permutation(self.samples_count)
-                    self.x = self.x[perm]
-                    self.y = self.y[perm]
-                batch_num = 0
+                    permuations = np.random.permutation(self.samples_count)
+                    self.x = self.x[permuations]
+                    self.y = self.y[permuations]
+                batchNum = 0
 
     def show(self):
-        """
-        Plot the top 16 images (index 0~15) of self.x for visualization.
-        """
-        X_sample = self.x[:16] # take all before index 16
-
+        
+        #Plot the top 16 images
+        X_sample = self.x[:16]
         # imshow() one channel of images
-        r = 4  # dimension of plots matrix 4 * 4
-        f, axarr = plt.subplots(r, r, figsize=(8,8))  # plot matrix and figure size
+        # dimension of plots matrix 4 * 4
+        r = 4 
+        # plot matrix and figure size
+        f, axarr = plt.subplots(r, r, figsize=(8,8))  
         for i in range(r):
             for j in range(r):
-                img = X_sample[r*i+j]
-                axarr[i][j].imshow(img, cmap="gray")  # grey images
+                image = X_sample[r*i+j]
+                axarr[i][j].imshow(image, cmap="gray")
 
-    def shift(self, height_shift, width_shift):
+    def shift(self, heightShift, widthShift):
         """
-        Translate self.x by the values given in shift.
-        :param height_shift: the number of pixels to shift along height direction. Can be negative.
-        :param width_shift: the number of pixels to shift along width direction. Can be negative.
-        :return:
+        translate self.x by height and width shift
+        height_shift: pixels to shift along height direction, can be negative
+        width_shift: pixels to shift along width direction, can be negative
         """
-        self.height_shift = height_shift
-        self.width_shift = width_shift
+        self.height_shift = heightShift
+        self.width_shift = widthShift
 
-        # shift the values along the axis, 1 and 2 means height and width
-        # Roll array elements along a given axis.
-        # Elements that roll beyond the last position are re-introduced at the first.
-        self.x = np.roll(self.x, height_shift, axis=1)  # shift in height, axis = 1, horizontal
-        self.x = np.roll(self.x, width_shift, axis=2)  # shift in width, axis = 2, vertical
+        #shift the values along axis 1 - height and axis 2 width, roll elements, 
+        # if rolled later than the last position, re-introduce at the first position
+        # shift in height, axis = 1, horizontal
+        self.x = np.roll(self.x, heightShift, axis=1)
+        # shift in width, axis = 2, vertical
+        self.x = np.roll(self.x, widthShift, axis=2) 
 
     def flip(self, mode='h'):
-        """
-        Flip self.x according to the mode specified
-        :param mode: 'h' or 'v' or 'hv'. 'h' means horizontal and 'v' means vertical.
-        Do the augmentation.
-        """
-        self.flip_horizontal = 'h' in mode  # h = horizontal, default horizontal
-        self.flip_vertical = 'v' in mode  # v = vertical
+        #flip self.x according to the specified mode, if h - horizontal, v - vertical
+        # h = horizontal, default horizontal
+        self.flip_horizontal = 'h' in mode  
+        # v = vertical
+        self.flip_vertical = 'v' in mode  
+        # flip horizontally ----- flip on axis 2
         if self.flip_horizontal:
-            self.x = np.flip(self.x, axis=2)  # flip horizontally is to flip upon axis 2
+            self.x = np.flip(self.x, axis=2) 
+        # flip vertically ---- flip upon axis 1 
         if self.flip_vertical:
-            self.x = np.flip(self.x, axis=1)  # flip vertically is to flip upon axis 1
+            self.x = np.flip(self.x, axis=1) 
