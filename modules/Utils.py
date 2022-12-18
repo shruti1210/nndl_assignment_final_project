0079ecc624683639362e8c432b0b392c3eb48d45
Shruti Sharma <shruti12101997@gmail.com>
2022-12-17 19:56:10 -0500

    utils.py

--- modules/Utils.py
+++ modules/Utils.py
@@ -92,11 +92,11 @@ def load_cifar100(get_test_data=True, channels_last=True):
 
     download_cifar10(download_100=True)
 
-    # load batches in order:
-    dirpath = os.path.join(BASE_DIR, 'cifar-100-python')
+    # load batches in order
+    directoryPath = os.path.join(BASE_DIR, 'cifar-100-python')
     images = None # No test data return
-    filename = 'train'
-    fpath = os.path.join(dirpath, filename)
+    fileName = 'train'
+    fpath = os.path.join(directoryPath, fileName)
     with open(fpath, 'rb') as f:
         content = pickle.load(f, encoding='bytes')
     if images is None:
@@ -115,36 +115,33 @@ def load_cifar100(get_test_data=True, channels_last=True):
     if not get_test_data:
         return images, labels
 
-    filename = 'test'
-    fpath = os.path.join(dirpath, filename)
+    fileName = 'test'
+    fpath = os.path.join(directoryPath, fileName)
     with open(fpath, 'rb') as f:
         content = pickle.load(f, encoding='bytes')
-    test_images = content[b'data'].reshape(-1, 3, 32, 32)
-    test_labels = np.asarray(content[b'fine_labels'])
+    testImages = content[b'data'].reshape(-1, 3, 32, 32)
+    testLabels = np.asarray(content[b'fine_labels'])
 
     # normalize data by dividing by 255:
-    test_images = test_images / 255.
+    testImages = testImages / 255.
     # Make channels last: ==> NHWC
     if channels_last:
-        test_images = np.moveaxis(test_images, 1, -1)
+        testImages = np.moveaxis(testImages, 1, -1)
 
-    return images, labels, test_images, test_labels
+    return images, labels, testImages, testLabels
 
 
-def strip_consts(graph_def, max_const_size=32):
-    '''
-    Strip large constant values from graph_def.
-    '''
-    strip_def = tf.GraphDef() # A protobuf containing the graph of operations.
+def stripConsts(graph_def, max_const_size=32):
+    stripDefination = tf.GraphDef()
     for n0 in graph_def.node:
-        n = strip_def.node.add()
-        n.MergeFrom(n0) # Merge the fields from the given message into this message.
+        n = stripDefination.node.add()
+        n.MergeFrom(n0)
         if n.op == 'Const':
-            tensor = n.attr['value'].tensor # Access tensor_content values in TensorProto
+            tensor = n.attr['value'].tensor
             size = len(tensor.tensor_content)
             if size > max_const_size:
                 tensor.tensor_content = "<stripped %d bytes>" % size
-    return strip_def
+    return stripDefination
 
 
 def show_graph(graph_def, max_const_size=32):
@@ -155,7 +152,7 @@ def show_graph(graph_def, max_const_size=32):
     if hasattr(graph_def, 'as_graph_def'):
         # as_graph_def() returns a serialized GraphDef representation of this graph.
         graph_def = graph_def.as_graph_def()
-    strip_def = strip_consts(graph_def, max_const_size = max_const_size)
+    stripDef = stripConsts(graph_def, max_const_size = max_const_size)
 
     code = """
         <script src="//cdnjs.cloudflare.com/ajax/libs/polymer/0.3.3/platform.js"></script>
@@ -168,7 +165,7 @@ def show_graph(graph_def, max_const_size=32):
         <div style="height:600px">
           <tf-graph-basic id="{id}"></tf-graph-basic>
         </div>
-    """.format(data=repr(str(strip_def)), id='graph' + str(np.random.rand()))
+    """.format(data=repr(str(stripDef)), id='graph' + str(np.random.rand()))
 
     iframe = """
         <iframe seamless style="width:1000px;height:620px;border:0" srcdoc="{}"></iframe>
